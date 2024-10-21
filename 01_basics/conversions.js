1b61bccc4c0331e85601209ff1b9312abdb9c1f2
kulpreet147 <kulpreet.singh@bentsys.com>
2024-10-21 06:15:58 +0000

    ssds

--- 01_basics/conversions.js
+++ 01_basics/conversions.js
@@ -1,7 +1,12 @@
-let value1 = 5;
-let value2 = "9";
-let value3 = 9;
+// let value1 = 5;
+// let value2 = "9";
+// let value3 = 9;
 
-let sum = value1 + value2 +value3;
+// let sum = value1 + value2 +value3;
 
-console.log(sum);
+// console.log(sum);
+
+// comarison conversions----------------------------------
+console.log(null > 0);//false bcz null is coerced to 0 and 0>0 is false
+console.log(null == 0);//false bcz here equality operator does not coerced so null is not equal to 0
+console.log(null >= 0);//true,bcz null coerced to 0 and 0>=0 is true
